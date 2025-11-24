bfa71adf679e61265c6eeb3cbcd2d8fe9c1bceb9
Malhar <malharkulkarni19@gmail.com>
2025-11-24 12:59:27 +0530

    Added more code

--- core_java/src/ClassesAndObjects.java
+++ core_java/src/ClassesAndObjects.java
@@ -2,9 +2,18 @@ class Student{
     int a;
     int b;
 
-    public int add(int a, int b) {
-        System.out.println("In add method");    //just for checking purpose
+    public int add(int a, int b) {     //this is behaviour of the method
+        System.out.println("In add method");
         return a + b;
+    }
+
+    public void getMeAPen(int price){
+        if(price <= 10) {
+            System.out.println("I want a Pen");
+        }
+        else{
+            System.out.println("I don't want a Pen");
+        }
     }
 }
 
@@ -16,5 +25,8 @@ public class ClassesAndObjects {
         Student a1 = new Student();
 
         System.out.println(a1.add(num1,num2));
+
+        Student s2 = new Student();
+        a1.getMeAPen(15);
     }
 }
