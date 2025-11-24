47a5f219e6398ed8ad531876a333702034fd4188
Malhar <malharkulkarni19@gmail.com>
2025-11-24 14:03:11 +0530

    Changes Commited

--- core_java/src/MethodOverloading.java
+++ core_java/src/MethodOverloading.java
@@ -7,6 +7,10 @@ class Calculator{
     public int add(int a, int b, int c){
         return a + b + c;
     }
+
+    public double add(double a, double b){
+        return a + b;
+    }
 }
 
 public class MethodOverloading {
@@ -16,8 +20,12 @@ public class MethodOverloading {
         int result1 = obj1.add(12,11);
         int result2 = obj1.add(12,11, 27);
 
+        Calculator obj2 = new Calculator();
+        double result3 = obj2.add(12.5,12.5);
+
         System.out.println(result1);
         System.out.println(result2);
+        System.out.println(result3);
 
     }
 }
